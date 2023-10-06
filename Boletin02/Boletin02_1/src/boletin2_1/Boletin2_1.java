7e1830b4722faa3a16a59008b28f529ccafdbac2
cristianregueiromartinez69 <cregueiromartinez@danielcastelao.org>
2023-10-06 10:05:23 +0200

    cambiando valores area

--- Boletin02/Boletin02_1/src/boletin2_1/Boletin2_1.java
+++ Boletin02/Boletin02_1/src/boletin2_1/Boletin2_1.java
@@ -2,7 +2,7 @@
 package boletin2_1;
 public class Boletin2_1 {
     public static void main(String[] args) {
-        float base=3,altura=4,area;
+        float base=5,altura=7,area;
         area=(base*altura)/2;
         System.out.println("El area del triangulo es: "+area);
     }
